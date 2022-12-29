76515ca335b91f93a35c2f3c30af9c786a07b096
bvbsaha <bvbsaha@gmail.com>
2022-12-30 00:48:34 +0300

    added firebase removed parcelize added extensions

--- app/src/main/java/com/bvbsaha/fitnesskursach/domain/PlayerModel.kt
+++ app/src/main/java/com/bvbsaha/fitnesskursach/domain/PlayerModel.kt
@@ -1,7 +1,8 @@
 package com.perpetio.squat.challenge.domain
 
 import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
+import kotlinx.android.parcel.Parcelize
+
 
 @Parcelize
 data class PlayerModel(val name: String = "Without name", val score: String= "0", val type : String = "squat") : Parcelable
